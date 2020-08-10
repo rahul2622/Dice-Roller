606f103f9a13227e600f506719d5114b2b601d0b
Rahul <rahulmore26229@gmail.com>
2020-08-11 00:34:01 +0530

    initial commit

---
+++ DiceRoller/App.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import { StyleSheet, Text, View , Image , TouchableOpacity} from 'react-native';
+
+export default class  App  extends React.Component{
+constructor(){
+  super();
+  this.state= {
+    uri:require("./src/images/dice1.png"),
+   
+  }
+}
+
+getRandomValue= () => {
+return Math.floor(Math.random() * 6) +1
+}
+
+
+playbuttonpressed= () => {
+ // Alert.alert("Heyy: " + this.getRandomValue())
+var rNumber = this.getRandomValue();
+switch (rNumber) {
+  case 1:
+    this.setState({uri:require("./src/images/dice1.png") })
+   break;
+
+   case 2:
+    this.setState({uri:require("./src/images/dice2.png") })
+   break;
+   case 3:
+    this.setState({uri:require("./src/images/dice3.png") })
+   break;
+   case 4:
+    this.setState({uri:require("./src/images/dice4.png") })
+   break;
+   case 5:
+    this.setState({uri:require("./src/images/dice5.png") })
+   break;
+   case 6:
+    this.setState({uri:require("./src/images/dice6.png") })
+   break;
+   
+
+  default:
+    this.setState({uri:require("./src/images/dice1.png") })
+    break;
+}
+
+}
+
+
+  render(){
+  return (
+    
+    
+    <View style={styles.container}>
+      <Image
+      source={this.state.uri}
+      style={styles.Image}
+      
+      ></Image>  
+      <Image
+      source={this.state.uri}
+      style={styles.Image2}
+      
+      ></Image>   
+       <TouchableOpacity
+       onPress={this.playbuttonpressed}
+       >
+      <Text style={styles.gamebutton}>
+      Play Game
+      </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+  }
+ const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#E74292',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  gamebutton: {
+    marginTop: 35,
+    fontSize:20,
+    color: "#FFFFFF",
+    fontWeight: "bold",
+    borderWidth:2,
+    paddingVertical:8,
+    paddingHorizontal: 40,
+    borderRadius:5,
+    borderColor: "#FFF" 
+
+  },
+  Image:{
+width:50,
+height:50
+
+  },
+  Image2:{
+    width:50,
+    height:50
+
+    
+      }
+});
